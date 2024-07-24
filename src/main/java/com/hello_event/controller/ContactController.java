4dd5d1755621583b1ef228ab2ac5fbe65a582807
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-07-24 12:25:36 +0100

    stocker les demandes de contact

--- src/main/java/com/hello_event/controller/ContactController.java
+++ src/main/java/com/hello_event/controller/ContactController.java
@@ -1,10 +1,11 @@
 package com.hello_event.controller;
 
+import com.hello_event.model.Contact;
 import com.hello_event.service.ContactService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user/contact")
@@ -12,5 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class ContactController {
     private final ContactService contactService;
-
+    @PostMapping
+    public ResponseEntity<Contact> saveTicket(@RequestBody Contact contact) {
+        Contact savedContact = contactService.save(contact);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedContact);
+    }
 }
